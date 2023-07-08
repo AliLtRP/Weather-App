f6125f50897c58fc0424503fed517e1c8bcb7344
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2023-07-08 23:56:59 +0300

    add Home

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 import SideNavBar from './components/SideNavBar/SideNavBar';
+import Home from './components/Home/Home';
 
 function App() {
   
   return (
-    <>
+    <div className='main'>
       <SideNavBar />
-    </>
+      <Home />
+    </div>
   );
 }
 
