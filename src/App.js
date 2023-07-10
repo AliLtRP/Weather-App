496d937c40db077642f9c161aab8338c91704c56
Ali Ahmed <aliahmed@Alis-MacBook-Air.local>
2023-07-11 00:00:29 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.css';
 import SideNavBar from './components/SideNavBar/SideNavBar';
 import Home from './components/Home/Home';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  
+  const[city, setCity] = useState('baghdad');
+  const[lat, setLat] = useState(0);
+  const[lon, setLon] = useState(0);
+
+  // useEffect(() => {
+  //   axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${city} &limit=1&appid=fdfea805c327b7df7d8f8c3ac49bba07`)
+  //   .then(res => {
+  //     setLat(res.data[0].lat);
+  //     setLon(res.data[0].lon);
+  //   })
+
+  // }, [city]);
+
+  // useEffect(() => {
+  //   axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${"fdfea805c327b7df7d8f8c3ac49bba07"}`)
+  //   .then(res => console.log(res.data))
+  // },[lat, lon]);
+
   return (
     <div className='main'>
       <SideNavBar />
